633ca7ee0389fd914da61c9f33e63423a7d68085
seonyeonghun2 <kjca1117@naver.com>
2024-11-29 18:29:43 +0900

    템플릿 엔진 | 코드 적용순서 및 레이아웃 변경

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 const path = require("path");
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 const mainRouter = require("./routes/mainRouter.js");
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('layout', 'layouts/main.ejs');
 app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", mainRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
