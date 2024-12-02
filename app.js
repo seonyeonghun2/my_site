1f6777184ad0296a7780e21972efa10ce1d084e7
seonyeonghun2 <kjca1117@naver.com>
2024-12-02 17:08:58 +0900

    mongodb 연결

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const dotenv = require("dotenv"); // .env 환경설정 파일을 읽어들이는 패키지
+dotenv.config(); // .env 설정된 환경변수와 값을 읽음
+const connectDB = require("./config/database.js");
 const expressLayouts = require("express-ejs-layouts");
 const mainRouter = require("./routes/mainRouter.js");
+
+connectDB(); // DB 연결 실행
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
