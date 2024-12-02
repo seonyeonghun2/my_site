09c166878f585b9afdfd7cb230e8242f53485cfd
seonyeonghun2 <kjca1117@naver.com>
2024-12-02 14:50:39 +0900

    메인(=공통) 라우터 | 레이아웃, 데이터 수정

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router();
-const mainLayout = require("../views/layouts/main.ejs");
+const mainLayout = "../views/layouts/main.ejs";
 
 router.get('/', (req, res) => {
-    const locals = {
-      title: 'Page Title',      
-      header: 'Page Header'
-    };
-    res.render('index', {layout: mainLayout, locals});
-  });
+  const locals = {
+    title: 'My Site ',
+    header: 'Special Header'
+  }
+  res.render('index', {locals, layout: mainLayout});
+});
 // router.get('/signup', (req, res) => {
 // // res.send('Hello World!')
 // res.render('join');
