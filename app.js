8c1def602656c929da31762869bb96b9b5d5466d
seonyeonghun2 <kjca1117@naver.com>
2024-12-02 14:49:42 +0900

    ejs 뷰 템플릿 적용 | 오류 수정

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const path = require("path");
 const expressLayouts = require('express-ejs-layouts');
 const mainRouter = require("./routes/mainRouter.js");
 
+app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set('layout', 'layouts/main');
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('layout', 'layouts/main.ejs');
-app.use(expressLayouts);
 app.use("/", mainRouter);
 
 app.listen(port, () => {
