3f34bd2a6877124749e3b75a230c0b98c684ace8
seonyeonghun2 <kjca1117@naver.com>
2024-12-04 17:29:12 +0900

    admin 라우터 | 폼데이터 처리 및 라우터 등록

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ dotenv.config(); // .env 설정된 환경변수와 값을 읽음
 const connectDB = require("./config/database.js");
 const expressLayouts = require("express-ejs-layouts");
 const mainRouter = require("./routes/mainRouter.js");
-
+const adminRouter = require("./routes/adminRouter.js");
 connectDB(); // DB 연결 실행
 
 app.use(expressLayouts);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set('layout', 'layouts/main');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", mainRouter);
+app.use("/", adminRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
