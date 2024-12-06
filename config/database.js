4f15fca09ff1e9b464155b1e196813536ec77283
seonyeonghun2 <kjca1117@naver.com>
2024-12-06 17:13:56 +0900

    디버깅 모드 | 설정

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
-
+mongoose.set("debug", true);
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log('=================== DB 연결 성공 =================')
-        if (!conn) {
-            console.log('=================== DB 연결 실패 =================')
-        }
-    } catch (err) {
-        console.log('=================== DB 연결 오류 =================')
-        console.log(err);
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      authSource: "admin",
+    });
+    console.log("=================== DB 연결 성공 =================");
+    if (!conn) {
+      console.log("=================== DB 연결 실패 =================");
     }
-}
+  } catch (err) {
+    console.log("=================== DB 연결 오류 =================");
+    console.log(err);
+  }
+};
 
 module.exports = connectDB;
