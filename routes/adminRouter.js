fec6be6c12707d42b6202ca970f214479a6c82b7
seonyeonghun2 <kjca1117@naver.com>
2024-12-06 17:14:22 +0900

    로그인 및 유지를 위한 쿠키, JWT 발급 로직

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET; // secretOrPrivate Key
+const cookieParser = require("cookie-parser");
 const mainLayout = "../views/layouts/main.ejs";
 const User = require("../models/User.js"); // node v18 이상은 확장자를 생략할수 없음.
 /**
@@ -19,66 +22,96 @@ router.get("/admin", (req, res) => {
  * POST /admin
  */
 router.post("/admin", async (req, res) => {
-  const locals = {
-    title: "My Site [Admin]",
-    header: "관리자 로그인",
-  };
-  const {user_id, user_pwd} = req.body;
-  
-  const user = await User.find({user_id});
+  try {
+    const { user_id, user_pwd } = req.body;
+    console.log(user_id, user_pwd);
+    const user = await User.find({ user_id });
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({ message: "존재하지 않는 사용자입니다"});
+    }
 
-  if (!user) {
-    throw new Error('user account is not found');
+    const isValid = bcrypt.compare(user_pwd, user.user_pwd);
+
+    if (!isValid) {
+      return res.status(401).json({ message: "아이디, 비밀번호를 다시 확인하세요"});
+    }
+
+    const token = jwt.sign({ id: user.user_id }, jwtSecret); // 관리자 토큰 (이름,로그인날짜,시간제한,..각종 정보)
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/allPosts");
+   
+  } catch (error) {
+    console.log(error);
   }
 
-  console.log(user);
-
-  res.render("admin/index", { locals, layout: mainLayout });
 });
+
+/**
+ * 관리자 전용 화면 : 작성한 전체 게시물 목록 화면 [수정,삭제,추가,목록...] + 로그아웃 [버튼]
+ * GET /allPosts
+*/
+
+const 토큰체크MW = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect('/admin'); // 토큰 값 없으면, 관리자 로그인 폼으로 리다이렉트
+  }
+  next();
+}
+
+router.get("/allPosts", 토큰체크MW, (req, res) => {
+  // 토큰 체크 : 관리자 유무 확인
+  
+  
+  res.render("admin/allPosts", { layout: mainLayout });
+})
+
+
 /**
  * 회원 가입 폼 보기
  * GET /register
  */
-router.get('/register', (req, res) => {
-    res.send("회원 가입 폼을 보여줍니다.")
-})
+router.get("/register", (req, res) => {
+  res.send("회원 가입 폼을 보여줍니다.");
+});
+
 /**
- * 회원 가입요청
+ * 관리자 회원 가입요청
  * POST /register
  */
-router.post('/register', async (req, res) => {
-    try {
-      const {user_id, user_nick, user_pwd, user_email} = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { user_id, user_nick, user_pwd, user_email } = req.body;
 
-      const hashedPwd = await bcrypt.hash(user_pwd, 10);      
-  
-      const user = new User({
-        user_id: user_id,
-        user_pwd: hashedPwd,
-        user_nick: user_nick,
-        user_email: user_email
-      });
+    const hashedPwd = await bcrypt.hash(user_pwd, 10);
 
-      const savedUser = await user.save();
+    const user = new User({
+      user_id: user_id,
+      user_pwd: hashedPwd,
+      user_nick: user_nick,
+      user_email: user_email,
+    });
 
-      if(!savedUser) {
-        throw new Error('계정 생성 실패!');
-      }
-         
-      res.status(200).send({
-        message: "user account is created",
-        data: user
-      });
+    const savedUser = await user.save();
 
-    } catch (error) {
-      console.log(error);
+    if (!savedUser) {
+      throw new Error("계정 생성 실패!");
     }
-})
+
+    res.status(200).send({
+      message: "user account is created",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 /**
  * 회원 정보 찾기
  * POST /find
  */
-router.post('/find', (req, res) => {
-    res.send("이름 또는 이메일정보로 회원 ID/PW 찾기")
-})
+router.post("/find", (req, res) => {
+  res.send("이름 또는 이메일정보로 회원 ID/PW 찾기");
+});
 module.exports = router;
