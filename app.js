2c8f43acc83013d8c8b62168639891277b061406
seonyeonghun2 <kjca1117@naver.com>
2024-12-13 16:51:38 +0900

    메소드 오버라이드 | 패키지 추가 및 미들웨어 등록

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 const port = 3000;
 const path = require("path");
@@ -15,6 +16,7 @@ app.use(expressLayouts);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set('layout', 'layouts/main');
